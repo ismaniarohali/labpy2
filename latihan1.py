d1d6fbfaae20e2faf6cb9303ad94bdd36f264d08
niaaarhl <ismaniarohali5@gmail.com>
2021-11-13 15:13:41 +0700

    menambahkan file

--- latihan1.py
+++ latihan1.py
@@ -1,13 +1,12 @@
-print ("Program Mencari Bilangan Terbesar Dari 3 Bilangan")
+print("Program Mencari Bilangan Terbesar Dari 3 Bilangan")
 
 a = int(input("Masukan Bilangan Pertama: "))
 b = int(input("Masukan Bilangan Kedua: "))
-c = int(input ("Masukan Bilangan Ketiga: "))
+c = int(input("Masukan Bilangan Ketiga: "))
 
-if a > b > c :
+if a > b > c:
     print("Bilangan pertama Adalah Terbesar = %s" % a)
-elif b > c :
+elif b > c:
     print("Bilangan Kedua Adalah Bilangan Terbesar = %s" % b)
-else :
+else:
     print("Bilangan ketiga Adalah Bilangan Terbesar = %s" % c)
-
